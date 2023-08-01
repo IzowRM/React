5075b49f8eae771201501807d24502975c6fb4cf
RobinMougne <robin.m@hotmail.fr>
2023-08-01 15:47:50 +0300

    Initial commit

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function DropdownMenu() {
+    const [selectedOption, setSelectedOption] = useState("option1");
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    return (
+        <div>
+            <select value={selectedOption} onChange={handleChange}>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+            </select>
+            <p>Selected Option: {selectedOption}</p>
+        </div>
+    );
+}
+export default DropdownMenu
